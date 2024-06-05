52dd87e457e0c55b49accc80c56aa8fc315b617c
Jeenyhus <mweembadabwitso@gmail.com>
2024-06-05 16:11:18 +0200

    added book and genre models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Book (models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    genre = models.ForeignKey('Genre', related_name='genre', on_delete=models.CASCADE)
+    
+class Genre (models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    
