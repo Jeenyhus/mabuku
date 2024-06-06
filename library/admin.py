714b2edb5a2eb8d86592aa7e8348e0aa36b4d55f
Jeenyhus <mweembadabwitso@gmail.com>
2024-06-06 02:40:49 +0200

    registered some of the new models

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Book, Genre, Librarian, Member
+from .models import Book, Genre, Librarian, Member, BookItem, LibraryCard, BookReservation, BookLending
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Librarian)
 admin.site.register(Member)
+admin.site.register(BookItem)
+admin.site.register(LibraryCard)
+admin.site.register(BookReservation)
+admin.site.register(BookLending)
