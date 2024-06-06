15e38e5862a51e478fd776309f9a6366d2bc8cc2
Jeenyhus <mweembadabwitso@gmail.com>
2024-06-06 04:20:33 +0200

    Registered some of the new models

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Genre, Librarian, Member, BookItem, LibraryCard, BookReservation, BookLending
+from .models import Book, Genre, Librarian, Member, BookItem, LibraryCard, BookReservation, BookLending, Catalog, Author, Rack, Library, Notification, PostalNotification, EmailNotification, BarcodeReader, Account, Transaction, FineTransaction, CreditCardTransaction, CheckTransaction, CashTransaction
 # Register your models here.
 
 admin.site.register(Book)
@@ -10,3 +10,16 @@ admin.site.register(BookItem)
 admin.site.register(LibraryCard)
 admin.site.register(BookReservation)
 admin.site.register(BookLending)
+admin.site.register(Author)
+admin.site.register(Rack)
+admin.site.register(Library)
+admin.site.register(Notification)
+admin.site.register(PostalNotification)
+admin.site.register(EmailNotification)
+admin.site.register(BarcodeReader)
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(FineTransaction)
+admin.site.register(CreditCardTransaction)
+admin.site.register(CheckTransaction)
+admin.site.register(CashTransaction)
